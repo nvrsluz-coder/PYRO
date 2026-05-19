6f21b34965d5c76b0153cf969108550d7d9ca789
nvrsluz-coder <nvrsluz@gmail.com>
2026-05-19 15:02:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,7 @@ import requests, os
 
 app = Flask(__name__)
 
-@app.route('/run')
-def run():
-    secret = request.headers.get("X-Secret")
-    if secret != os.getenv("SECRET_TOKEN"):
-        abort(403)
-
+def get_token():
     api_dev_key = os.getenv("API_DEV_KEY")
     username = os.getenv("PASTEBIN_USERNAME")
     password = os.getenv("PASTEBIN_PASSWORD")
@@ -20,13 +15,22 @@ def run():
         "api_user_password": password
     }).text
 
-    code = requests.post("https://pastebin.com/api/api_raw.php", data={
+    token = requests.post("https://pastebin.com/api/api_raw.php", data={
         "api_dev_key": api_dev_key,
         "api_user_key": user_key,
         "api_option": "show_paste",
         "api_paste_key": paste_key
     }).text
 
+    return token.strip()
+
+@app.route('/run')
+def run():
+    secret = request.headers.get("X-Secret")
+    if secret != os.getenv("SECRET_TOKEN"):
+        abort(403)
+    
+    code = get_token()
     return code
 
 if __name__ == '__main__':
